db513fda85ffaa2f0109133a076c44bb52d99319
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-04-09 10:49:49 -0700

    add priority to tasks

--- src/index.js
+++ src/index.js
@@ -9,3 +9,10 @@ testTask.switchComplete();
 console.log(testTask.completionStatus);
 testTask.switchComplete();
 console.log(testTask.completionStatus);
+console.log(`Priority: ${testTask.priority}`);
+testTask.priority = 3;
+console.log(`Priority: ${testTask.priority}`);
+try { testTask.priority = 0; } catch (e) { console.log(e.message); }
+try { testTask.priority = "hello"; } catch (e) { console.log(e.message); }
+let secondTask = new Task("Second Task", "we're on a roll", 2);
+console.log(`Second Task Priority: ${secondTask.priority}`);
--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,13 +1,29 @@
 export default class Task {
-    #complete = false;
+    #complete = false; // Starts off incomplete always; can only switch through provided function
+    #priority; // User shouldn't be able to directly set this except through provided setter, thus it's private
 
-    constructor(name, description) {
+    constructor(name, description = '', priority = 4) {
         this.name = name;
         this.description = description;
+        this.#priority = priority;
     }
 
     get completionStatus() {
         return this.#complete;
+    }
+
+    get priority() {
+        return this.#priority;
+    }
+
+    set priority(level) { // Some of this might be irrelevant in practice through browser usage but I still want it covered
+        if (typeof (level) != "number") {
+            throw new TypeError("Priority level should be a number");
+        }
+        if (level < 1 || level > 4) {
+            throw new RangeError("Priority level should be between 1 and 4");
+        }
+        this.#priority = level;
     }
 
     switchComplete() {
